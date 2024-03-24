139a2372b808142c4744b00c6f28af1c23dc78b7
GGantsios <ggantsios@gmail.com>
2024-03-24 20:12:42 +0200

    24/3/2024. Step 1 fpadd_single.v - Gantsios

--- step1/verilog_input/fpadd_single.v
+++ step1/verilog_input/fpadd_single.v
@@ -32,56 +32,63 @@ module fpadd_single (input clk,
 					out <= result;
 				end
 		end
-		
 	//Combinational Logic to (a) compare and adjust the exponents, 
 	//                       (b) shift appropriately the mantissa if necessary, 
 	//                       (c) add the two mantissas, and
 	//                       (d) perform post-normalization. 
 	//                           Make sure to check explicitly for zero output. 
+
+	reg [31:0] A, B;
+	reg sign_A, sign_B;
+	reg [7:0] exp_A, exp_B;
+	reg [23:0] mantissa_A, mantissa_B;
+	reg [24:0] sum;
+	reg [31:0] result;
+
+	wire max_sign = (exp_A > exp_B) ? sign_A :
+					(mantissa_A > mantissa_B) ? sign_A :
+					sign_B;
+
+	wire max_exp = (exp_A > exp_B) ? exp_A : exp_B;
+
 	always@ (*)
+	begin
+		// Extract the sign, exponent, and mantissa of A and B
+		// Sign
+		sign_A = A[31];
+		sign_B = B[31];
+		// Exponent
+		exp_A = A[30:23];
+		exp_B = B[30:23];
+		// Mantissa
+		mantissa_A = {1'b1, A[22:0]};
+		mantissa_B = {1'b1, B[22:0]};
+	end
+
+	always @(*) 
+	begin
+		mantissa_A_shifted = (exp_A < exp_B) ? mantissa_A : mantissa_A >> (exp_B - exp_A);
+		mantissa_B_shifted = (exp_A > exp_B) ? mantissa_B : mantissa_B >> (exp_A - exp_B);
+		new_exp = (exp_A > exp_B) ? sign_A : sign_B;
+	end
+
+	always @(*)
+	begin
+		sum = (sign_A ^ sign_B)    ? mantissa_A_shifted + mantissa_B_shifted :
+			  (sign_A & (~sign_B)) ? mantissa_A_shifted - mantissa_B_shifted :
+			  (~(sign_A) & sign_B) ? mantissa_B_shifted - mantissa_A_shifted ;
+	end
+
+	always @(*)
+	begin
+		if(sum[24])
 		begin
-			// Extract the sign, exponent, and mantissa of A and B
-			// Sign
-			assign sign_A = A[31];
-			assign sign_B = B[31];
-			// Exponent
-			assign exp_A = A[30:23];
-			assign exp_B = B[30:23];
-			// Mantissa
-			assign mantissa_A = A[22:0];
-			assign mantissa_B = B[22:0];
-			
-			assign final_mantissa_A = (sign_A == 1'b1) ? -mantissa_A : mantissa_A;
-			assign final_mantissa_B = (sign_B == 1'b1) ? -mantissa_B : mantissa_B;
-
-			if (exp_A > exp_B)
-				begin
-					// Shift the mantissa of B to the right by exp_A-exp_B bits
-					assign mantissa_B_shifted = final_mantissa_B >> (exp_A - exp_B);
-					// The new exponent is the same as the larger exponent
-					assign new_exp = exp_A;
-					// The new mantissa is the sum of the two mantissas 
-					assign sum = final_mantissa_A + mantissa_B_shifted;		
-				end				
-			else if (exp_A < exp_B)
-				begin
-					// Shift the mantissa of A to the right by exp_B-exp_A bits
-					assign mantissa_A_shifted = final_mantissa_A >> (exp_B - exp_A);
-					// The new exponent is the same as the larger exponent
-					assign new_exp = exp_B;
-					// The new mantissa is the sum of the two mantissas
-					assign sum = mantissa_A_shifted + final_mantissa_B;		
-				end
-			else
-				begin
-					// The new exponent is the same as the larger exponent
-					assign new_exp = exp_A;
-					// The new mantissa is the sum of the two mantissas
-					assign sum = final_mantissa_A + final_mantissa_B;		
-				end
-
-				
+			result = {max_sign, max_exp, sum[23:1]};
 		end
-
+		else
+		begin
+			result = {max_sign, max_exp, sum[22:0]};
+		end
+	end
 
 endmodule
